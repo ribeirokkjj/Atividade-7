1c81b4f5f0b8b6356293885462b3269e7e884c90
Antonio <antonio_ribeiro-n@estudante.sesisenai.org.br>
2026-03-20 09:58:45 -0300

    Added 7 and 9 questions

--- exercicio_7.js
+++ exercicio_7.js
@@ -1,4 +1,44 @@
 function question7() {
-  
-    
+    let codigo = prompt("Escolha o picolé pelo código para saber o preço: \n a - Chocolate\n b - Morango\n c - Creme \n d - Manga\n e - Melancia \n f - Vanilla Ice \n g - Céu Azul \n h - Brownie\n i - Hawaiano")
+    let valor = null;
+
+    switch (codigo){
+        case "a":
+            valor = 1.5
+            break;
+                    case "b":
+            valor = 2.5
+            break;
+                    case "c":
+            valor = 2.5
+            break;
+                    case "d":
+            valor = 3.2
+            break;
+                    case "e":
+            valor = 3.4
+            break;
+                    case "f":
+            valor = 3
+            break;
+                    case "g":
+            valor = 3.6
+            break;
+                    case "h":
+            valor = 4
+            break;
+                    case "i":
+            valor = 5
+            break;
+    }
+    if (valor == null) {
+        alert("Escolha um código existente!")
+    }
+
+    if (valor != null) {
+        alert("O valor do picolé é R$"+valor)
+    }
+
 }
+
+
--- exercicio_9.js
+++ exercicio_9.js
@@ -1,3 +1,7 @@
 function question9() {
-    // função vazia por enquanto
+    let n1= [10,20]
+
+    alert("Valores: " + n1)
+    n1.sort((a, b) => b - a)
+    alert("Valores organizados: " + n1)
 }
